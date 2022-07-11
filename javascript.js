6c20ce18e174ed4211d22238cb8e5e6c1f0e8510
Juan Correa <juancorrea50@yahoo.com>
2022-07-11 04:50:26 -0500

    Add h1 element, functional rainbow button, and user friendly design
    Title element added for description of program. Fully functional
    rainbow button added. Replaces divs from previous grid with another grid
    with the same dimensions, just added different color values to
    the logic.

--- javascript.js
+++ javascript.js
@@ -3,17 +3,17 @@ let userWidth=16;
 
 const container = document.querySelector('.container');
 const gridBtn = document.querySelector('.grid-dimensions');
+const rainbowButton = document.querySelector('.rainbow');
 
 
-
-//Grid logic in a function
+//Creates a basic grid
 function createGrid(){
-     //Creates divs using for loop
+     //Creates divs using a for loop
     for(x=0;x<(userWidth);x++){
         for(y=0;y<(userHeight);y++){
             //Divs created in memory and appended at the end of the for loop
             const divs = document.createElement('div');
-            divs.style.cssText = 'background-color: black;';
+            divs.style.cssText = 'background-color: white;';
             
             divs.style.height = `${800/userHeight}px`;
             divs.style.width = `${800/userWidth}px`;
@@ -21,18 +21,18 @@ function createGrid(){
             // *Hover effect*
             //Mouse enter
              divs.addEventListener("mouseenter", (e) =>{
-                 e.target.style.backgroundColor = "white";
+                 e.target.style.backgroundColor = "black";
         
                 setTimeout(() => {
-                    e.target.style.backgroundColor="black";
+                    e.target.style.backgroundColor="white";
                 }, 500);
             })
              //Mouse over
             divs.addEventListener("mouseover", (e)=> {
-                e.target.style.backgroundColor = "white";
+                e.target.style.backgroundColor = "black";
         
                 setTimeout(() => {
-                    e.target.style.backgroundColor = "black";
+                    e.target.style.backgroundColor = "white";
                 }, 500);
             });
 
@@ -41,6 +41,42 @@ function createGrid(){
         }
     
      }
+}
+
+
+//Creates a grid where the square divs have random hex values
+function createRainbowGrid(){
+   for(x=0;x<(userWidth);x++){
+       for(y=0;y<(userHeight);y++){
+
+            const divs = document.createElement('div');
+            divs.style.cssText = 'background-color: white;';
+           
+            divs.style.height = `${800/userHeight}px`;
+            divs.style.width = `${800/userWidth}px`;
+            //Random Color logic
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+            divs.addEventListener("mouseenter", (e) =>{
+                e.target.style.backgroundColor = '#' + randomColor;
+       
+               setTimeout(() => {
+                   e.target.style.backgroundColor="white";
+               }, 500);
+            })
+
+            divs.addEventListener("mouseover", (e)=> {
+               e.target.style.backgroundColor = '#' + randomColor;
+       
+               setTimeout(() => {
+                   e.target.style.backgroundColor = "white";
+               }, 500);
+            });
+
+           container.appendChild(divs);
+       }
+   
+    }
 }
 
 //initialize grid to 16x16
@@ -56,13 +92,21 @@ gridBtn.addEventListener('click', () =>{
     do{
        
         userHeight = prompt("Choose the height of the grid(under 100)", 0);
-    }while(userHeight>100);
+    }while(userHeight>100 || userHeight===0 || typeof userHeight !== 'number');
 
     do{
        
         userWidth =prompt("Choose the width of the grid(under 100)", 0);
-    }while(userWidth> 100);
+    }while(userWidth> 100 || userWidth===0 || typeof userWidth !== 'number');
 
     //Create new grid elements
     createGrid();
+});
+
+//Rainbow button event listener
+rainbowButton.addEventListener('click', () => {
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    createRainbowGrid();
 });
